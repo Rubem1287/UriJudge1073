a3898095dff54c1937414402d64addab05272b57
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:39:38 -0300

    Add files via upload

---
+++ Uri1073/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uri1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,raiz;
+
+            raiz = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= n; c++)
+            {
+                if (c%2==0)
+                {
+                    raiz = (int)Math.Pow(c, 2);
+                    Console.WriteLine(c + "^2 = " + raiz);
+                }
+            }
+
+        }
+    }
+}
